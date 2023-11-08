ef7e948dfacc64cb4734965c6d64d442c28a0929
Deyterriy <justkozmail@gmail.com>
2023-11-08 22:23:19 +0300

    Разработан бэк

--- backend/foodgram/api/serializer.py
+++ backend/foodgram/api/serializer.py
@@ -22,7 +22,7 @@ class Base64ImageField(serializers.ImageField):
             format, imgstr = data.split(';base64,')
             ext = format.split('/')[-1]
             data = ContentFile(base64.b64decode(imgstr), name='photo.' + ext)
-        
+
         return super().to_internal_value(data)
 
 
