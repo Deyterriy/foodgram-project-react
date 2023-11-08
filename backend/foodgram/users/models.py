e1fa68eee8569d80be3b1e115872c61d3c888803
Deyterriy <justkozmail@gmail.com>
2023-11-08 20:30:37 +0300

    Создан БЭК ЗАДАНИЯ

---
+++ backend/foodgram/users/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """Модель пользователей"""
+
+    email = models.EmailField(
+        verbose_name='Электронная почта',
+        unique=True
+    )
+    username = models.CharField(
+        max_length=150,
+        verbose_name='Имя пользователя',
+        unique=True,
+        db_index=True
+    )
+    first_name = models.CharField(
+        max_length=150,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=150,
+        verbose_name='Фамилия'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        """Строковое представление модели пользователя"""
+
+        return self.username
