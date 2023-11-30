cec39f8fd9edc899e440af310b27e389d880a81b
Kirill Kozlov <justkozmail@gmail.com>
2023-11-30 03:54:25 +0300

    Обработка несуществующего рецепта и пользователя

--- backend/api/views.py
+++ backend/api/views.py
@@ -129,7 +129,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     @staticmethod
     def delete_recipe(request, Models, pk, **kwargs):
-        recipe = get_object_or_404(Recipe, id=kwargs['pk'])
+        try:
+            recipe = Recipe.objects.get(id=kwargs['pk'])
+        except Recipe.DoesNotExist:
+                raise ValidationError(
+                    'Рецепт с указанным идентификатором не существует.'
+                )
         get_object_or_404(Models, user=request.user,
                           recipe=recipe).delete()
 
@@ -160,7 +165,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
         recipe = get_object_or_404(Recipe, id=kwargs['pk'])
 
         if request.method == 'POST':
-            recipe = get_object_or_404(Recipe, id=kwargs['pk'])
+            try:
+                recipe = Recipe.objects.get(id=kwargs['pk'])
+            except Recipe.DoesNotExist:
+                raise ValidationError(
+                    'Рецепт с указанным идентификатором не существует.'
+                )
             self.create_recipe(recipe, request, ShoppingCart)
             return Response({'errors': 'Рецепт уже в списке покупок.'},
                             status=status.HTTP_400_BAD_REQUEST)
